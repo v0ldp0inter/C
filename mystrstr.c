eff19edecb2fcdcf3ecfa1ed04708317b383f23d
v0ldp0inter <34393211+v0ldp0inter@users.noreply.github.com>
2017-12-09 08:22:25 -0200

    Create mystrstr.c

---
+++ mystrstr.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+char *my_strstr(char *st, char *p){
+   if (*st == 0x0) return NULL; 
+   char *j = NULL;
+   long c = 0;
+   
+    while(*st){
+        j = p;
+        while(*j != 0x0 && *st == *j){
+          ++j;
+          ++c;
+          ++st;
+        }
+          if(*j == 0x0){
+            return st - c;
+          }
+        
+      ++st;
+    }
+  
+  return NULL;
+}
+
+int main(int argc, char **argv){
+  puts(my_strstr("atheist", "ist"));
+  return 0;
+}
