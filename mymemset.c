22fb62ede51235fc6e07eb9d45c980d05f5db460
Y. V. Saint <34393211+v0ldp0inter@users.noreply.github.com>
2018-03-31 04:58:50 -0300

    Create mymemset.c

---
+++ mymemset.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+void *_mymemset(void *st, char rew, size_t len){
+     char *aux = (char *)st;
+     size_t i;
+    
+     for(i = 0; i <= len; ++i)
+         aux[i] = rew;
+  
+  return aux;
+}
+
+int main(int argc, char **argv){
+  char aux[] = "teste";
+  puts(aux);
+  
+  _mymemset(aux, 0x0, sizeof(aux));
+  puts(aux);
+  return 0;
+}
